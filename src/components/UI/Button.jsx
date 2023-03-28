8a92e1a86a69718f19144042ccabba55289c6b8d
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2023-03-28 23:13:50 +0200

    themes done

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -4,39 +4,33 @@ export default function Button({
   big,
   textStyle,
   BGcolor,
-  shadowColor,
-  shadowColorDark,
-  shadowColorRed,
   white,
   dark,
   red,
   dispatch,
   digit,
+  position,
 }) {
   return (
     <button
       onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
       className={`${BGcolor} ${big} ${
-        white
-          ? "hover:bg-theme1-whiteHover"
-          : dark
-          ? "hover:bg-theme1-darkHover"
-          : red
-          ? "hover:bg-theme1-redHover"
-          : ""
-      } rounded-[10px] flex justify-center hover:cursor-pointer items-center ${
-        shadowColor
-          ? "shadow-[inset_0_-4px_0_rgba(179,164,151)]"
-          : shadowColorDark
-          ? "shadow-[inset_0_-4px_0_rgba(65,78,115)]"
-          : shadowColorRed
-          ? "shadow-[inset_0_-4px_0_rgba(147,38,26)]"
-          : ""
-      } `}
+        position === 2
+          ? "hover:bg-theme2-digitHover"
+          : position === 3
+          ? "hover:bg-theme3-digitHover"
+          : "hover:bg-theme1-digitHover"
+      } rounded-[10px] flex justify-center hover:cursor-pointer items-center 
+      ${
+        position === 2
+          ? "shadow-[inset_0_-4px_0_rgba(167,158,145)]"
+          : position === 3
+          ? "shadow-[inset_0_-4px_0_rgba(136,28,158)]"
+          : "shadow-[inset_0_-4px_0_rgba(179,164,151)]"
+      } 
+      `}
     >
       <p className={`${textStyle} text-center my-3`}>{digit}</p>
     </button>
   );
 }
-
-//pt-[3px] mt-[13px] mb-[11px] w-[74px] h-[40px]
--- src/components/UI/OperationButton.jsx
+++ src/components/UI/OperationButton.jsx
@@ -1,9 +1,35 @@
 import { ACTIONS } from "../../App";
 
-export default function OperationButton({ dispatch, operation }) {
+export default function OperationButton({ dispatch, operation, position }) {
   return (
     <button
-      className="text-h2 bg-theme1-button rounded-[10px] hover:cursor-pointer hover:bg-theme1-whiteHover shadow-[inset_0_-4px_0_rgba(179,164,151)]"
+      className={`${
+        position === 2
+          ? "text-theme2-pureBlack"
+          : position === 3
+          ? "text-theme3-yellow"
+          : "text-theme1-digit"
+      }  text-h2  ${
+        position === 2
+          ? "bg-theme2-button"
+          : position === 3
+          ? "bg-theme3-button"
+          : "bg-theme1-button"
+      } rounded-[10px] hover:cursor-pointer hover:bg-theme1-whiteHover ${
+        position === 2
+          ? "shadow-[inset_0_-4px_0_rgba(167,158,145)]"
+          : position === 3
+          ? "shadow-[inset_0_-4px_0_rgba(136,28,158)]"
+          : "shadow-[inset_0_-4px_0_rgba(179,164,151)]"
+      }  
+      ${
+        position === 2
+          ? "hover:bg-theme2-digitHover"
+          : position === 3
+          ? "hover:bg-theme3-digitHover"
+          : "hover:bg-theme1-digitHover"
+      }
+      `}
       onClick={() =>
         dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
       }
